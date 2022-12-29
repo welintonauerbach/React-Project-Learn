4db00de87da52b702020ad74544ccd42d6d9a663
Welinton Zanotto Auerbach <welinton.auerbach@gmail.com>
2022-12-29 10:55:12 -0300

    Ajustado export Function

--- src/modules/app-comp-frame/app-control-frame.jsx
+++ src/modules/app-comp-frame/app-control-frame.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 // App frame container - Exemplo
-export default props =>
-<section className="container" hidden={!props.appEnable}>
-<div className="app-control-frame">
-    <h3>{props.frameName}</h3>
-    {props.children}
-</div>
-</section>
+const AppControlFrame = props => (
+
+    <section className="container" hidden={!props.appEnable}>
+        <div className="app-control-frame">
+            <h3>{props.frameName}</h3>
+            {props.children}
+        </div>
+    </section>);
+
+export default AppControlFrame;
